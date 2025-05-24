dd981fb4481b52db047e307fe73b475feb116fe4
Ephraim Bennett <ephraimbennett330@gmail.com>
2025-05-24 14:26:19 -0500

    Added light/dark mode

--- finder/static/dashboard.js
+++ finder/static/dashboard.js
@@ -2,3 +2,22 @@ function toggleMenu() {
     let menu = document.querySelector(".side-menu");
     menu.classList.toggle("open");
 }
+
+function toggleTheme() {
+    
+    document.body.classList.toggle('dark-mode');
+    const savedTheme = localStorage.getItem('theme');
+    const isDark = savedTheme === 'dark';
+
+    localStorage.setItem('theme', document.body.classList.contains('dark-mode') ? 'dark' : 'light');
+    var btn = document.getElementById("theme-toggle");
+    if (btn) btn.textContent = isDark ? '☀️' : '🌙';
+
+}
+
+// Auto-apply saved theme
+window.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem('theme') === 'dark') {
+        document.body.classList.add('dark-mode');
+    }
+});
